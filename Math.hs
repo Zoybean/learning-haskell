b9ff08752f81b16e3a199523cc283c5adeed3d1d
Zoey Llewellyn Hewll <ZoeyHewll@gmail.com>
2017-02-04 14:37:30 +0800

    documented functions, added fib

--- Math.hs
+++ Math.hs
@@ -1,19 +1,25 @@
 module Math where
 
-hcf :: Int -> Int -> Int
+fibs :: [Int] -- Fibonacci sequence
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+fib :: Int -> Int -- Fibonacci number
+fib n = fibs !! n
+
+hcf :: Int -> Int -> Int -- Highest common factor
 hcf 0 y = y
 hcf x y
     | x < 0 = hcf (negate x) y
     | x <= y = hcf x (y - x)
     | otherwise = hcf y x
 
-lb :: Int -> Maybe Int
+lb :: Int -> Maybe Int -- Binary logarithm, rounded down
 lb 1 = Just 0
 lb x
     | x > 1 = fmap (+1) $ lb (div x 2)
     | otherwise = Nothing
 
-lbRnd :: Int -> Maybe Int
+lbRnd :: Int -> Maybe Int -- Binary logarithm, rounded nearest
 lbRnd 1 = Just 0
 lbRnd x = let
             m :: Floating b => Int -> b
@@ -23,7 +29,7 @@ lbRnd x = let
           in
             if fmap ((fromIntegral x)<) (mid x) == Just True then lb x else lbMax x
 
-lbMax :: Int -> Maybe Int
+lbMax :: Int -> Maybe Int -- Binary logarithm, rounded up
 lbMax 1 = Just 0
 lbMax x
     | mod x 2 == 0 = fmap (+1) $ lbMax (div x 2)
