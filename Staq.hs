a31f50007251158129ef477b6fec9027cf5b7bf9
Zoey Llewellyn Hewll <ZoeyHewll@gmail.com>
2017-02-22 19:17:03 +0800

    replaced liftA2 with <$>, <*>

--- Staq.hs
+++ Staq.hs
@@ -1,6 +1,5 @@
 module Staq where
 import List (intercalate)
-import Control.Applicative (liftA2)
 -- queue (kinda) efficiently implemented as 2 stacks
 type Staq a = ([a],[a])
 newStaq :: [a] -> Staq a
@@ -34,6 +33,6 @@ main = do
     print $ q
     print $ flush q
     let nth q n = iterate (>>= rest) (Just q) !! n
-    let showStaq q = liftA2 (\d r -> intercalate ":" [show d, show r]) (deq q) (rest q)
-    mapM_ print $ map (\n -> (nth q n) >>= showStaq) [0,1,2,3,4,5]
+    let showStaq q = (\d r -> intercalate ":" [show d, show r]) <$> (deq q) <*> (rest q)
+    mapM_ print $ (\n -> (nth q n) >>= showStaq) <$> [0,1,2,3,4,5]
     
